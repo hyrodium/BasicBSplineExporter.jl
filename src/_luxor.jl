89fa06cbda3d1a2d8abf6ed1af23583f9607451a
hyrodium <hyrodium@gmail.com>
2021-12-29 10:50:25 +0900

    refactor _luxor.jl

--- src/_luxor.jl
+++ src/_luxor.jl
@@ -9,95 +9,85 @@ function _bezier(f,a,b)
 end
 
 function _luxor_pt(p::AbstractVector{<:Real}, unitlength)
-    Point(unitlength*[1,-1].*p...)
+    Luxor.Point(unitlength*[1,-1].*p...)
 end
 
 """
 export svg file
 """
 function save_svg(name::String, M::BSplineManifold{1}; xlims=(-5,5), ylims=(-5,5), mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0))
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "svg"
         name = name * ".svg"
     end
-    _save_luxor_1d2d(name, M, up=up, down=down, right=right, left=left, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
+    _save_luxor_1d2d(name, M, xlims=xlims, ylims=ylims, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
 end
 
 """
 export svg file
 """
 function save_svg(name::String, M::BSplineManifold{2}; xlims=(-5,5), ylims=(-5,5), mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0))
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "svg"
         name = name * ".svg"
     end
-    _save_luxor_2d2d(name, M, up=up, down=down, right=right, left=left, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
+    _save_luxor_2d2d(name, M, xlims=xlims, ylims=ylims, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
 end
 
 """
 export png file
 """
 function save_png(name::String, M::BSplineManifold{1}; xlims=(-5,5), ylims=(-5,5), mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0))
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "png"
         name = name * ".png"
     end
-    _save_luxor_1d2d(name, M, up=up, down=down, right=right, left=left, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
+    _save_luxor_1d2d(name, M, xlims=xlims, ylims=ylims, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
 end
 
 """
 export png file
 """
 function save_png(name::String, M::BSplineManifold{2}; xlims=(-5,5), ylims=(-5,5), mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0))
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "png"
         name = name * ".png"
     end
-    _save_luxor_2d2d(name, M, up=up, down=down, right=right, left=left, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
+    _save_luxor_2d2d(name, M, xlims=xlims, ylims=ylims, mesh=mesh, unitlength=unitlength, points=points, thickness=thickness, backgroundcolor=backgroundcolor, maincolor=maincolor)
 end
 
 """
 export png file
 """
 function save_png(name::String, M::BSplineManifold{2}, colors::AbstractArray{<:Colorant,2}; xlims=(-5,5), ylims=(-5,5), unitlength=100)
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "png"
         name = name * ".png"
     end
 
-    _save_luxor_2d2d_color(name, M, colors, up=up, down=down, right=right, left=left, unitlength=unitlength)
+    _save_luxor_2d2d_color(name, M, colors, xlims=xlims, ylims=ylims, unitlength=unitlength)
 end
 
 """
 export png file
 """
 function save_png(name::String, M::BSplineManifold{2}, colorfunc::Function; xlims=(-5,5), ylims=(-5,5), unitlength=100)
-    left, right = xlims
-    down, up = ylims
     if split(name,'.')[end] ≠ "png"
         name = name * ".png"
     end
 
-    _save_luxor_2d2d_color(name, M, colorfunc, up=up, down=down, right=right, left=left, unitlength=unitlength)
+    _save_luxor_2d2d_color(name, M, colorfunc, xlims=xlims, ylims=ylims, unitlength=unitlength)
 end
 
 
-function _save_luxor_2d2d(name::String, M::BSplineManifold{2}; up=5, down=-5, right=5, left=-5, mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0), subcolor=RGB(0.5,0.5,0.5))
+function _save_luxor_2d2d(name::String, M::BSplineManifold{2}; xlims=(-5,5), ylims=(-5,5), mesh=(10,10), unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0), subcolor=RGB(0.5,0.5,0.5))
+    left, right = xlims
+    down, up = ylims
     linecolor = maincolor
     fillcolor = weighted_color_mean(0.5, maincolor, colorant"white")
     segmentcolor = subcolor
     pointcolor = weighted_color_mean(0.5, subcolor, colorant"black")
 
-    P1, P2 = P = collect(bsplinespaces(M))
-    p¹, p² = p = degree.(P)
-    k¹, k² = k = knotvector.(P)
+    P = bsplinespaces(M)
+    p = degree.(P)
+    k = knotvector.(P)
     a = controlpoints(M)
-    n¹, n² = n = dim.(P)
+    n = dim.(P)
 
     K¹, K² = K = [unique(k[i][1+p[i]:end-p[i]]) for i in 1:2]
     N¹, N² = length.(K).-1
@@ -129,10 +119,10 @@ function _save_luxor_2d2d(name::String, M::BSplineManifold{2}; up=5, down=-5, ri
 
         setcolor(segmentcolor)
         setline(thickness)
-        for i in 1:n¹
+        for i in 1:n[1]
             poly(CtrlPts[i,:], :stroke)
         end
-        for j in 1:n²
+        for j in 1:n[2]
             poly(CtrlPts[:,j], :stroke)
         end
 
@@ -143,20 +133,23 @@ function _save_luxor_2d2d(name::String, M::BSplineManifold{2}; up=5, down=-5, ri
     return nothing
 end
 
-function _save_luxor_1d2d(name::String, M::BSplineManifold{1}; up=5, down=-5, right=5, left=-5, mesh=10, unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0), subcolor=RGB(.5,.5,.5))
+function _save_luxor_1d2d(name::String, M::BSplineManifold{1}; xlims=(-5,5), ylims=(-5,5), mesh=10, unitlength=100, points=true, thickness=1, backgroundcolor=RGB(1,1,1), maincolor=RGB(1,0,0), subcolor=RGB(.5,.5,.5))
+    left, right = xlims
+    down, up = ylims
     linecolor = maincolor
     segmentcolor = subcolor
     pointcolor = weighted_color_mean(0.5, subcolor, colorant"black")
 
-    P1, = P = collect(bsplinespaces(M))
-    p¹, = p = degree.(P)
-    k¹, = k = knotvector.(P)
     a = controlpoints(M)
-    n¹, = n = dim.(P)
 
-    K¹, = K = [unique(k[i][1+p[i]:end-p[i]]) for i in 1:1]
-    N¹, = length.(K).-1
-    m¹, = mesh
+    P¹ = bsplinespaces(M)[1]
+    p¹ = degree(P¹)
+    k¹ = knotvector(P¹)
+    n¹ = dim(P¹)
+ 
+    K¹ = unique(k¹[1+p¹:end-p¹])
+
+    N¹ = length(K¹)-1
 
     Drawing(unitlength*(right-left),unitlength*(up-down),name)
     Luxor.origin(-unitlength*left,unitlength*up)
@@ -164,37 +157,35 @@ function _save_luxor_1d2d(name::String, M::BSplineManifold{1}; up=5, down=-5, ri
     background(backgroundcolor)
 
     setcolor(linecolor)
-    drawbezierpath(BezierPath([BezierPathSegment(map(p->_luxor_pt(p,unitlength),_bezier(u¹->M(u¹),K¹[i],K¹[i+1]))...) for i in 1:N¹]),:stroke)
+    drawbezierpath(BezierPath([BezierPathSegment(map(q->_luxor_pt(q,unitlength),_bezier(u¹->M(u¹),K¹[i],K¹[i+1]))...) for i in 1:N¹]),:stroke)
 
     if points
-        CtrlPts = [_luxor_pt(a[i,:],unitlength) for i in 1:size(a)[1]]
+        CtrlPts = [_luxor_pt(a[i,:],unitlength) for i in 1:n¹]
 
         setcolor(segmentcolor)
         setline(thickness)
         poly(CtrlPts[:], :stroke)
 
         setcolor(pointcolor)
-        map(p->circle(p,3*thickness,:fill), CtrlPts)
+        map(q->circle(q,3*thickness,:fill), CtrlPts)
     end
     finish()
     return nothing
 end
 
-function _save_luxor_2d2d_color(name::String, M::BSplineManifold{2}, colors::AbstractArray{<:Colorant,2}; up=5, down=-5, right=5, left=-5, unitlength=100)
+function _save_luxor_2d2d_color(name::String, M::BSplineManifold{2}, colors::AbstractArray{<:Colorant,2}; xlims=(-5,5), ylims=(-5,5), unitlength=100)
     C = CustomBSplineManifold(colors, bsplinespaces(M))
-    _save_luxor_2d2d_color(name, M, C; up=up, down=down, right=right, left=left, unitlength=unitlength)
+    _save_luxor_2d2d_color(name, M, C; xlims=xlims, ylims=ylims, unitlength=unitlength)
 end
 
-function _save_luxor_2d2d_color(name::String, M::BSplineManifold{2}, colorfunc; up=5, down=-5, right=5, left=-5, unitlength=100)
+function _save_luxor_2d2d_color(name::String, M::BSplineManifold{2}, colorfunc; xlims=(-5,5), ylims=(-5,5), unitlength=100)
+    left, right = xlims
+    down, up = ylims
     mesh = 10
 
-    P = collect(bsplinespaces(M))
-    p¹, p² = p = degree.(P)
-    k¹, k² = k = knotvector.(P)
-    a = controlpoints(M)
-    n¹, n² = n = dim.(P)
-
-    D = [k[i][1+p[i]]..k[i][end-p[i]] for i in 1:2]
+    P = bsplinespaces(M)
+    p¹, p² = degree.(P)
+    k¹, k² = knotvector.(P)
 
     K¹ = unique(vcat([collect(range(k¹[i], k¹[i+1], length=mesh+1)) for i in 1+p¹:length(k¹)-p¹-1]...))
     K² = unique(vcat([collect(range(k²[i], k²[i+1], length=mesh+1)) for i in 1+p²:length(k²)-p²-1]...))
@@ -205,10 +196,10 @@ function _save_luxor_2d2d_color(name::String, M::BSplineManifold{2}, colorfunc; 
 
     for I₁ in 1:length(K¹)-1, I₂ in 1:length(K²)-1
         BézPth=BezierPath([
-                BezierPathSegment(map(p->_luxor_pt(p,unitlength),_bezier(t->M(t,K²[I₂]),K¹[I₁],K¹[I₁+1]))...),
-                BezierPathSegment(map(p->_luxor_pt(p,unitlength),_bezier(t->M(K¹[I₁+1],t),K²[I₂],K²[I₂+1]))...),
-                BezierPathSegment(map(p->_luxor_pt(p,unitlength),_bezier(t->M(t,K²[I₂+1]),K¹[I₁+1],K¹[I₁]))...),
-                BezierPathSegment(map(p->_luxor_pt(p,unitlength),_bezier(t->M(K¹[I₁],t),K²[I₂+1],K²[I₂]))...)])
+                BezierPathSegment(map(q->_luxor_pt(q,unitlength),_bezier(t->M(t,K²[I₂]),K¹[I₁],K¹[I₁+1]))...),
+                BezierPathSegment(map(q->_luxor_pt(q,unitlength),_bezier(t->M(K¹[I₁+1],t),K²[I₂],K²[I₂+1]))...),
+                BezierPathSegment(map(q->_luxor_pt(q,unitlength),_bezier(t->M(t,K²[I₂+1]),K¹[I₁+1],K¹[I₁]))...),
+                BezierPathSegment(map(q->_luxor_pt(q,unitlength),_bezier(t->M(K¹[I₁],t),K²[I₂+1],K²[I₂]))...)])
         mesh1 = Luxor.mesh(BézPth, [
             colorfunc(K¹[I₁],   K²[I₂]  ),
             colorfunc(K¹[I₁+1], K²[I₂]  ),
